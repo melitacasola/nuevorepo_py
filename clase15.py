bfd5c26ae2b6260388853d3e02e484f5aa50ae78
Meli Casola <melissa.casola@mi.unc.edu.ar>
2022-03-23 20:39:35 -0300

    agregue un archivo y ahroa le dejo un mensaje que modifique

---
+++ clase15.py
@@ -0,0 +1,18 @@
+#import sys
+
+from datetime import datetime
+
+# if len(sys.argv) == 3:
+#     cadena = sys.argv[1]
+#     repeticiones = int(sys.argv[2])
+#     for r in range(repeticiones):
+#         print(cadena)
+#         #print(sys.argv)
+        
+# else:
+#     print("ERROR! introduce los argumentos correctamente")
+#     print('Ejempl archivo.py "texto" Nro')
+    
+dt = datetime.now()
+
+print(dt)
